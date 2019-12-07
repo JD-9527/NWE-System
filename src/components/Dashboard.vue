ae5cc8172147e26c69b5b1840676f072a8adc0c9
r041291867 <r041291867@gmail.com>
2019-12-07 08:54:18 +0800

    add login & dashboard file

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{Dashboard}}</div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    Dashboard: 'Dashboard'
+  }),
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>Login</div>
+</template>
+
+<script>
+import { Toast } from 'mint-ui';
+
+export default {
+  data () {
+    return {
+      userName: '',
+      password: '',
+    }
+  },
+  methods: {
+    login(){
+      //write login authencation logic here!
+      if( this.userName == 'nwe' && this.password == '123456' ){
+        localStorage.setItem('token', 'ImLogin')
+        Toast('登录成功！');
+        this.$router.push('/jphadmin');
+      } else{
+        Toast('账号或密码错误！');
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
