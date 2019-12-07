243ee8ffac51cce8ffe7455acad77f8731c7038c
r041291867 <r041291867@gmail.com>
2019-12-07 08:55:08 +0800

    add vuerouter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px" style="background-color: #123456">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#123456"
+          text-color="#eee"
+          active-text-color="#486dd5"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>生產狀態</span>
+            </template>
+              <el-menu-item index="1-1">生產總覽</el-menu-item>
+              <el-menu-item index="1-2">工單看板</el-menu-item>
+              <el-menu-item index="1-3">機台看板</el-menu-item>
+              <el-menu-item index="1-4">計畫看板</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-s-data"></i>
+              <span>生產統計</span>
+            </template>
+              <el-menu-item index="2-1">機台性能</el-menu-item>
+              <el-menu-item index="2-2">生產利潤</el-menu-item>
+              <el-menu-item index="2-3">其他查詢</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3">
+            <i class="el-icon-date"></i>
+            <span slot="title">生產排程</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-upload"></i>
+            <span slot="title">數據錄入</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <Dashboard/>
+        </el-main>
+        <el-footer
+          height=30
+        >
+          {{ new Date().getFullYear() }} — &copy; IAI Innovative Application Dept.
+        </el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Dashboard
+  },
+  data:() => ({
+      
+  })
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.el-header {
+    background-color: #123456;
+    color: #eee;
+    line-height: 60px;
+  }
+.el-main {
+  hight: 100%;
+}
+.el-aside {
+  color: #eee;
+}
+.el-footer {
+  background-color: #eee;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui';
+import { Toast } from 'mint-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+import Login from './components/Login'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(ElementUI);
+Vue.component(Toast.name, Toast);
+
+// const Login = { template: '<div>login</div>'}
+const routes = [
+	{ path: '/login', component: Login }
+]
+
+const router = new VueRouter({
+	routes
+})
 
 new Vue({
 	el: '#app',
+	router,
   render: h => h(App),
 }).$mount('#app')
