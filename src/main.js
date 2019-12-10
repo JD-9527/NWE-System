52bc64ebd84145710667cc61e2fc15a1c7d82285
r041291867 <r041291867@gmail.com>
2019-12-10 14:59:15 +0800

    add echarts

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ElementUI from 'element-ui';
-import { Toast } from 'mint-ui';
+import myCharts from './components/myCharts.js'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
+import MachineBoard from './components/MachineBoard'
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 Vue.use(ElementUI);
-Vue.component(Toast.name, Toast);
+Vue.use(myCharts);
 
 // const Login = { template: '<div>login</div>'}
 const routes = [
 	{ path: '/login', component: Login },
-	{ path: '/dashboard', component: Dashboard }
+	{ path: '/dashboard', component: Dashboard },
+	{ path: '/machine', component: MachineBoard }
 ]
 
 const router = new VueRouter({
