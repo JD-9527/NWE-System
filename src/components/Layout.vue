25147b0492070a1af626e683687596c7b22508c2
r041291867 <r041291867@gmail.com>
2020-01-07 16:49:59 +0800

    機台看板路徑修改

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -36,7 +36,7 @@
             </template>
               <el-menu-item index="/overview/dashboard">生產總覽</el-menu-item>
               <el-menu-item index="/overview/work_order">工單看板</el-menu-item>
-              <el-menu-item index="/overview/machine">機台看板</el-menu-item>
+              <el-menu-item index="/overview/machine/D10">機台看板</el-menu-item>
               <el-menu-item index="/overview/plan">計畫看板</el-menu-item>
           </el-submenu>
           <el-submenu index="2">
