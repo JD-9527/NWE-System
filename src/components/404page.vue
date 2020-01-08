0ce932d7645d08c0a0e366594de93392fac0b032
r041291867 <r041291867@gmail.com>
2020-01-08 09:11:49 +0800

    修改404頁面

--- src/components/404page.vue
+++ src/components/404page.vue
@@ -1,29 +1,29 @@
 <template>
-  <el-row>
-    <el-col :span="1">
-      <el-button
-        type="text"
-        @click="$router.go(-1)"
-        style="font-size: 20px; color: #424242;"
-      >
-        <i class="el-icon-back"></i>返回
-      </el-button>
-    </el-col>
-    <el-col :span="22">
-      <div class="title">
-       404 Error
-      </div>
-      <div class="highlight">
-        頁面不存在
-      </div>
-      <div class="sub-title">
-        您可以點擊"返回"回到上一頁
-      </div>
-      <div class="sub-title">
-        或點擊側邊欄任意一項回到正確路徑
-      </div>
-    </el-col>
-  </el-row>
+  <div style="height: 100%">
+    <el-row>
+      <el-col :span="1">
+        <el-button
+          type="text"
+          @click="$router.go(-1)"
+          style="font-size: 20px; color: #424242;"
+        >
+          <i class="el-icon-back"></i>返回
+        </el-button>
+      </el-col>
+      <el-col :span="21">
+        <div class="title">
+         404 Error
+        </div>
+        <div class="highlight">
+          施工中
+        </div>
+        <div class="sub-title">
+          您可以點擊"返回"回到上一頁<br/><br/>
+          或點擊側邊欄任意一項回到正確路徑
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -48,7 +48,7 @@ export default {
 .highlight {
   font-size: 64px;
   text-align: center;
-  line-height: 500px;
+  line-height: 600px;
   font-weight: bold;
   color: #aaa;
 }
