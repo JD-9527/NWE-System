29c2fbcd14e7d5e790e2aab3408632343e1d9efd
JasonTai <r041291867@gmail.com>
2020-05-15 17:02:32 +0800

    add jquery

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/nwe/'
-    : '/'
+    : '/',
+    configureWebpack: {
+      plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          'windows.jQuery': 'jquery',
+        }),
+      ],
+    },
 }
